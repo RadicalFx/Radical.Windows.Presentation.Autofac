63156db607b0e1ea41534f2ffa22b18e4d48ee18
Mauro Servienti <mauro@topics.it>
2015-06-27 19:23:09 +0200

    fixing build

--- src/net40/Radical.Windows.Presentation.Autofac/Properties/AssemblyInfo.cs
+++ src/net40/Radical.Windows.Presentation.Autofac/Properties/AssemblyInfo.cs
@@ -2,7 +2,11 @@
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle( "Radical.Windows.Presentation.Autofac" )]
-[assembly: AssemblyDescription( "" )]
+[assembly: AssemblyDescription( "Radical is an infrastructure framework whose primary role is to help in the development of composite WPF applications based on the Model View ViewModel pattern." )]
+[assembly: AssemblyCompany( "topics.it" )]
+[assembly: AssemblyProduct( "Radical Framework" )]
+[assembly: AssemblyCopyright( "Copyright ©  2015" )]
+[assembly: AssemblyTrademark( "" )]
 [assembly: AssemblyConfiguration( "" )]
 [assembly: AssemblyCulture( "" )]
 
