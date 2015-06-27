a3abeb137e65a743df89581d163c8e827fc1f0fc
Mauro Servienti <mauro@topics.it>
2015-06-27 15:05:53 +0200

    initial commit

---
+++ src/net40/Radical.Windows.Presentation.Autofac/Boot/AutofacApplicationBootstrapper (Generic).cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Windows;
+using Topics.Radical.Windows.Presentation.ComponentModel;
+
+namespace Topics.Radical.Windows.Presentation.Boot
+{
+	public class AutofacApplicationBootstrapper<TShellView> :
+		AutofacApplicationBootstrapper 
+		where TShellView : Window
+	{
+		public AutofacApplicationBootstrapper()
+		{
+			this.UsingAsShell<TShellView>();
+		}
+	}
+}
